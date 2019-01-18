fd4eadf5916e58a62da63f3f687c1c21df40b3ab
mcferrenm <mcferrenmax@gmail.com>
2019-01-18 12:00:52 -0500

    Update selectTabHandler to pass tab name and set to app state

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -22,7 +22,7 @@ export default class Content extends Component {
 
   changeSelected = tab => {
     // this function should take in the tab and update the state with the new tab.
-    console.log("changeSelected fired");
+    this.setState({ selected: tab });
   };
 
   filterCards = () => {
--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -9,6 +9,7 @@ const Tabs = props => {
         <span className="title">TRENDING TOPICS:</span>
         {props.tabs.map(tab => (
           <Tab
+            key={tab}
             tab={tab}
             selectedTab={props.selectedTab}
             selectTabHandler={props.selectTabHandler}
