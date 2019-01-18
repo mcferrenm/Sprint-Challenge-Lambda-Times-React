ca181b877bc6fa5ed0b4559ffbf9aa9a9c8ef918
mcferrenm <mcferrenmax@gmail.com>
2019-01-18 14:15:56 -0500

    Modal content and styles tweaks

--- lambdatimes/src/components/LoginModal.js
+++ lambdatimes/src/components/LoginModal.js
@@ -32,16 +32,9 @@ class LoginModal extends React.Component {
           toggle={this.toggleModal}
           className={this.props.className}
         >
-          <ModalHeader toggle={this.toggleModal}>Modal title</ModalHeader>
-          <ModalBody>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-          </ModalBody>
+          <ModalHeader toggle={this.toggleModal}>
+            Login to Lambda Times
+          </ModalHeader>
           <ModalFooter>
             <Button color="primary" onClick={this.handleLogIn}>
               Log In
