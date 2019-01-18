229ea30281612ba0001d18d91be02bbc86073f95
mcferrenm <mcferrenmax@gmail.com>
2019-01-18 12:47:41 -0500

    Convert TopBar into styled-components

--- lambdatimes/src/components/TopBar.js
+++ lambdatimes/src/components/TopBar.js
@@ -1,24 +1,37 @@
-import React from 'react';
+import React from "react";
 
-// Refactor this component to use styled components and not classNames. 
+import {
+  TopBarWrapper,
+  Container,
+  ContainerLeft,
+  ContainerCenter,
+  ContainerRight
+} from "./Styles/TopBar/TopBarStyles";
+
+// Refactor this component to use styled components and not classNames.
 // You can find the corresponding CSS in the CSS/index.css file
 
 const TopBar = () => {
   return (
-    <div className="top-bar">
-      <div className="container">
-        <div className="container-left">
-          <span>TOPICS</span><span>SEARCH</span>
-        </div>
-        <div className="container-center">
-          <span>GENERAL</span><span>BROWNBAG</span><span>RANDOM</span><span>MUSIC</span><span>ANNOUNCEMENTS</span>
-        </div>
-        <div className="container-right">
+    <TopBarWrapper>
+      <Container>
+        <ContainerLeft>
+          <span>TOPICS</span>
+          <span>SEARCH</span>
+        </ContainerLeft>
+        <ContainerCenter>
+          <span>GENERAL</span>
+          <span>BROWNBAG</span>
+          <span>RANDOM</span>
+          <span>MUSIC</span>
+          <span>ANNOUNCEMENTS</span>
+        </ContainerCenter>
+        <ContainerRight>
           <span>LOG IN</span>
-        </div>
-      </div>
-    </div>
-  )
-}
+        </ContainerRight>
+      </Container>
+    </TopBarWrapper>
+  );
+};
 
 export default TopBar;
