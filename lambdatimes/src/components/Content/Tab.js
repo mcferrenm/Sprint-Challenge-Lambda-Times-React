5b5e478858741bcd4c7ede3a73c7485cd5622db1
mcferrenm <mcferrenmax@gmail.com>
2019-01-18 12:14:34 -0500

    Render cards to screen, include PropTypes for Cards and Card components

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -2,9 +2,6 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const Tab = props => {
-  /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
-      if they match, the className should be: 'tab active-tab', 
-      if it is not it should just be 'tab'*/
   const isSelectedTabClass =
     props.selectedTab === props.tab ? "tab active-tab" : "tab";
 
